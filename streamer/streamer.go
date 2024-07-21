d8edbae0e6ffd3827b0f80c611e4bbb0d56c3c61
Maxim <toper.one111@gmail.com>
2024-07-21 10:34:46 +0300

    feat: add the logic for the encode fn

--- streamer/streamer.go
+++ streamer/streamer.go
@@ -1,5 +1,12 @@
 package streamer
 
+import (
+	"fmt"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
 type ProcessingMessage struct {
 	ID         int
 	Successful bool
@@ -21,8 +28,8 @@ type Video struct {
 	OutputDir    string
 	EncodingType string
 	NotifyChan   chan ProcessingMessage
-	Options *VideoOptions
-	Encoder Processor
+	Options      *VideoOptions
+	Encoder      Processor
 }
 
 type VideoOptions struct {
@@ -39,17 +46,61 @@ func (vd *VideoDispatcher) NewVideo(id int, input, output, encType string, notif
 	}
 
 	return Video{
-		ID: id,
-		InputFile: input,
-		OutputDir: output,
+		ID:           id,
+		InputFile:    input,
+		OutputDir:    output,
 		EncodingType: encType,
-		NotifyChan: notifyChan,
-		Encoder: vd.Processor,
-		Options: ops,
+		NotifyChan:   notifyChan,
+		Encoder:      vd.Processor,
+		Options:      ops,
 	}
 }
 
-func (v *Video) encode() {}
+func (v *Video) encode() {
+	var fileName string
+
+	switch v.EncodingType {
+	case "mp4":
+		name, err := v.encodeToMP4()
+		if err != nil {
+			v.sendToNotifyChan(false, "", fmt.Sprintf("encode failed for %d: %s", v.ID, err.Error()))
+			return
+		}
+
+		fileName = fmt.Sprintf("%s.mp4", name)
+	default:
+		v.sendToNotifyChan(false, "", fmt.Sprintf("error processing for %d: invalid encoding type", v.ID))
+	}
+
+	v.sendToNotifyChan(true, fileName, fmt.Sprintf("vide id %d processed and saved as %s", v.ID, fmt.Sprintf("%s/%s", v.OutputDir, fileName)))
+}
+
+func (v *Video) encodeToMP4() (string, error) {
+	baseFileName := ""
+
+	if !v.Options.RenameOutput {
+		b := path.Base(v.InputFile)
+		baseFileName = strings.TrimSuffix(b, filepath.Ext(b))
+	} else {
+		// TODO: Generate random file name
+	}
+
+	err := v.Encoder.Engine.EncodeToMP4(v, baseFileName)
+	if err != nil {
+		return "", err
+	}
+
+	return baseFileName, nil
+}
+
+func (v *Video) sendToNotifyChan(successful bool, fileName, message string) {
+	v.NotifyChan <- ProcessingMessage{
+		ID:         v.ID,
+		Successful: successful,
+		Message:    message,
+		OutputFile: fileName,
+	}
+}
 
 func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
 	workerPool := make(chan chan VideoProcessingJob, maxWorkers)
